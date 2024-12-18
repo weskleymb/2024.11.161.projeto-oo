2ba078c1371bae5d2f890162beda27b541814c68
Weskley Bezerra <weskleymb@hotmail.com>
2024-12-18 14:40:28 -0300

    initial commit

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,19 @@ import { Carro } from './classes/carro.js';
 import { Conta } from './classes/conta.js';
 
 const irineu = new Conta(1, 'Irineu', 50);
-
-console.log(irineu.saldo);
+const erick = new Conta(2, 'Erick', 100);
 
 console.log(irineu.saldo);
 
 irineu.depositar(100);
 
 console.log(irineu.saldo);
+
+irineu.sacar(50);
+
+console.log(irineu.saldo);
+
+erick.transferir(irineu, 50);
+
+console.log(irineu.saldo);
+console.log(erick.saldo);
